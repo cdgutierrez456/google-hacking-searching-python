2adb9e15f88fcc0d77e046ed1cf3b30fa18dda38
Cristian Gutierrez <cdgutierrez456@gmail.com>
2026-03-17 21:44:30 -0500

    Configurando argumentos y aplicando logica para el caso donde se deba crear el archivo .env

--- ninjadorks.py
+++ ninjadorks.py
@@ -1,18 +1,53 @@
-from dotenv import load_dotenv
+from dotenv import load_dotenv, set_key
+import argparse
+import sys
 import os
 
 from googlesearch import GoogleSearch
 
-# Cargamos las variables en el entorno
-load_dotenv()
+def env_config():
+    """Configurar el archivo .env con los valores proporcionados"""
+    api_key = input("Introduce tu API_KEY de Google")
+    set_key(".env", "API_KEY_SERPAPI", api_key)
 
-# Leemos la clave API
-API_KEY_SERPAPI = os.getenv('API_KEY_SERPAPI')
+def main(query, configure_env, start_page, pages, lang):
+    # Comprobamos si existe el fichero .env
+    env_exist = os.path.exists(".env")
+    if not env_exist or configure_env:
+        env_config()
+        print("Archivo .env configurado satisfactoriamente")
+        sys.exit(1)
 
-query = 'filetype:sql "MySQL dump" (pass|password|passwd|pwd)'
+    # Cargamos las variables en el entorno
+    load_dotenv()
 
-gsearch = GoogleSearch(API_KEY_SERPAPI)
+    # Leemos la clave API
+    API_KEY_SERPAPI = os.getenv('API_KEY_SERPAPI')
 
-results = gsearch.search(query)
+    query = 'filetype:sql "MySQL dump" (pass|password|passwd|pwd)'
 
-print(results)
+    gsearch = GoogleSearch(API_KEY_SERPAPI)
+
+    results = gsearch.search(query)
+
+    print(results)
+
+if __name__ == "__main__":
+    # Configuracion de los argumentos del programa
+    parser = argparse.ArgumentParser(description="Esta herramienta premite realizar Hacking con buscadores de manera automatica.")
+    parser.add_argument("-q", "--query", type=str,
+                        help="Especifica el dork que dessea buscar \nEjemplo: -q 'filetype:sql \"MySQL dump\" (pass|password|passwd|pwd)'")
+    parser.add_argument("-c", "--configure", action="store_true",
+                        help="Incia el proceso de configuracion del archivo .env.\nUtiliza esta opcion sin otros argumentos para configurar las claves.")
+    parser.add_argument("--start-page", type=int, default=1,
+                        help="Define la pagina de inicio del buscador para obtener los resultados")
+    parser.add_argument("--pages", type=int, default=1,
+                        help="Numero de paginas de resultados de busqueda.")
+    parser.add_argument("--lang", type=str, default="lang_es",
+                        help="Codigo de idioma para los resultados de busqueda. Por defecto es 'lang_es'")
+    args = parser.parse_args()
+    main(query=args.query,
+         configure_env=args.configure,
+         pages=args.pages,
+         start_page=args.start_page,
+         lang=args.lang)
